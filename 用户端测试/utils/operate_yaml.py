9089d594523f4e7ce9dd8945347de68b638cef1a
张雅蓉 <zhangyarong@udesk.cn>
2023-07-28 18:03:51 +0800

    用户端测试L1

---
+++ 用户端测试/utils/operate_yaml.py
@@ -0,0 +1,14 @@
+import os
+import yaml
+
+
+class OperateYaml:
+    @staticmethod
+    def read_yaml(path):
+        cur_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        target_path = os.path.join(cur_dir, path)
+        with open(target_path, encoding='utf-8') as f:
+            results = yaml.safe_load(f)
+            results = results.get("search")["datas"]
+            return results
+
